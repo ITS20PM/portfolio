c1f0ba97cce822e9059ff13d391b652448d7d786
ITS20PM <patrick21meng@gmail.com>
2023-11-01 21:44:17 -0700

    update

---
+++ src/Components/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className="bg-gradient-to-r from-green-400 to-blue-800 ...">
+        <div>Nav</div>
+    </div>
+    
+  )
+}
+
+export default Nav
