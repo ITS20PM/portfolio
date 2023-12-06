8e529807c450a70c20a7351e483ddbdcfb7b02c1
ITS20PM <patrick21meng@gmail.com>
2023-12-06 10:57:20 -0800

    new version

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -10,6 +10,36 @@ const Projects = () => {
         <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
             <p className='text-4xl font-bold'>Projects</p>
             
+            <ul className='mt-5'>
+              <li className='font-mono text-2xl font-medium'>Weather App</li>
+              <li className='font-mono text-sm font-medium indent-6'>Tech Stack: HTML, CSS, JavaScript</li>
+              <li className='font-mono text-sm font-medium indent-6'>Available at: <a href = "https://its20pm.github.io/Weather_app/html/index.html" 
+                  className='underline-offset-1'>
+                Here</a></li>
+            </ul>
+
+            <ul className='mt-5'>
+              <li className='font-mono text-2xl font-medium'>To Do List App</li>
+              <li className='font-mono text-sm font-medium indent-6'>Tech Stack: React.js, Node.js, Express.js, MongoDB</li>
+            </ul>
+
+            <ul className='mt-5'>
+              <li className='font-mono text-2xl font-medium'>Blog Post App</li>
+              <li className='font-mono text-sm font-medium indent-6'>Tech Stack: Django, HTML, CSS, MySQL</li>
+            </ul>
+
+            <ul className='mt-5'>
+              <li className='font-mono text-2xl font-medium'>Email Spam Detector</li>
+              <li className='font-mono text-sm font-medium indent-6'>Tech Stack: Python, Pandas, Numpy</li>
+            </ul>
+
+            <ul className='mt-5'>
+              <li className='font-mono text-2xl font-medium'>Emotion Detection Tool</li>
+              <li className='font-mono text-sm font-medium indent-6'>Tech Stack: Python, NumPy, Keras, Tensorflow, Matplotlib</li>
+            </ul>
+
+            
+
         </div>
         
     </div>
